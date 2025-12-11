7e4d356b0a17d71b82e4620d504c30ddef10bea0
giriarte <a.g.iriarte@gmail.com>
2025-12-11 14:23:51 -0800

    Historical data for backtesting

--- tests/backtest.py
+++ tests/backtest.py
@@ -41,23 +41,25 @@ class BaseBacktestingStrat(Strategy):
         super().init()
         self.signal1 = self.I(STRATEGY)
         self.ratio = 1.5
-        self.risk_perc = 0.004
+        self.risk_perc = 0.009
 
     def next(self):
         super().next() 
         if self.signal1==1:
             sl1 = self.data.Close[-1] - self.data.Close[-1]*self.risk_perc
             tp1 = self.data.Close[-1] + (self.data.Close[-1]*self.risk_perc)*self.ratio
-            self.buy(size = 100, sl=sl1, tp=tp1)
+            self.buy(size = 3, sl=sl1, tp=tp1)
         elif self.signal1==2:
             sl1 = self.data.Close[-1] + self.data.Close[-1]*self.risk_perc
             tp1 = self.data.Close[-1] - (self.data.Close[-1]*self.risk_perc)*self.ratio
-            self.sell(size = 100, sl=sl1, tp=tp1)
+            self.sell(size = 3, sl=sl1, tp=tp1)
 
-data_path = os.path.abspath('tests\historicalData1m')
+data_path = os.path.abspath('tests\\historicalData1h\\forex')
     
 if not os.path.isdir(data_path):
     raise FileNotFoundError(f"Directory not found: {data_path}")
+
+result_summary = []
 
 for filename in os.listdir(data_path):
     # Check if the file ends with .csv
@@ -66,10 +68,8 @@ for filename in os.listdir(data_path):
         
         print(f"Reading file: {filename}")
 
-
-
         raw_data = pd.read_csv(file_path, index_col=0, parse_dates=True)
-        raw_data.index = pd.to_datetime(raw_data.index, format="%d.%m.%Y %H:%M:%S.%f %Z")
+        raw_data.index = pd.to_datetime(raw_data.index, format="%d.%m.%Y %H:%M:%S.%f")
         raw_data['Open'] = pd.to_numeric(raw_data['Open'], errors='coerce')
         raw_data['Close'] = pd.to_numeric(raw_data['Close'], errors='coerce')
         raw_data['High'] = pd.to_numeric(raw_data['High'], errors='coerce')
@@ -86,8 +86,26 @@ for filename in os.listdir(data_path):
         # raw_data = yf.download(tickers='BTC-USD', period='1y', interval='1d')
         # raw_data.columns = [col[0] for col in raw_data.columns]
 
-        bt = Backtest(raw_data, BaseBacktestingStrat, cash=100000, commission=.0002, margin=REQUIRED_MARGIN)
+        bt = Backtest(raw_data, BaseBacktestingStrat, cash=1000000, commission=.0002, margin=REQUIRED_MARGIN)
         stat = bt.run()
         print(stat)
         print(stat._trades)
-        bt.plot()
+        plotfilename = os.path.join("plots", filename)
+        bt.plot(filename=plotfilename)
+
+        file_stats = {
+            'filename': filename,
+            'Return perc': stat['Return [%]'],
+            'Equity final': stat['Equity Final [$]'],
+            '# Trades': stat['# Trades'],
+            'Win Rate perc': stat['Win Rate [%]'],
+            'Avg trade perc': stat['Avg. Trade [%]'],
+            'Sharpe Ratio': stat['Sharpe Ratio'],
+            'Calmar Ratio': stat['Calmar Ratio']
+        }
+        result_summary.append(file_stats)
+
+summary_df = pd.DataFrame(result_summary)
+
+print("\n--- Summary DataFrame ---")
+print(summary_df)
